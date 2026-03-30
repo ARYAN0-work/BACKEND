38d41ab96ef85559addcfaa72089fe7637a776e6
ARYAN0-WORK <carrer1270@gmail.com>
2026-03-30 17:58:34 +0530

    installition of nodemon

--- first_pkg/app.js
+++ first_pkg/app.js
@@ -7,3 +7,7 @@ const _= require('lodash') // it have the method that effectivly will just spit 
 const items =[1,[2,[3,[4]]]]
 const newItems = _.flattenDeep(items)// we have acces to flattenDeep method bcz we install loadash dependcie
 console.log(newItems);
+console.log('hello '); // testing nodemon
+
+// i manipulated pkg.json see two changes in script =>start and dev start casullay start app.js dev start the server with refrersh power i mean now we are using nodemon each and evry resfresh pe server restart with chabges toh ab hume baar baar node app.js nhi likhna padhega 
+
