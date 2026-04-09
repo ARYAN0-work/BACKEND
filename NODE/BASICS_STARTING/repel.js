17c3ef6d033093242ca98496bef18ae17e61b0ab
Aryan Mishra <carrer1270@gmail.com>
2026-04-09 21:57:27 +0530

    improving notes for better understanding

---
+++ NODE/BASICS_STARTING/repel.js
@@ -0,0 +1,27 @@
+//const amount = 12
+//
+//if (amount < 10) {
+//    console.log('small number');
+//}
+//
+//else{
+//    console.log('large number');
+//}
+//
+//console.log(`hey its my first node app!!!`);
+//
+
+////////// MADE FOR TELLING HOW TO USE REPEL
+
+// WAP TO HAVE AMOUNT 12 AND IF AMOUNT IS LESS THEN 10 PRINT SMALL NO. AND ELSE PRINT LARGE NO.
+
+const amount = 12;
+
+if (amount >10) {
+    console.log("large no.");
+    
+}
+
+else{
+    console.log("small no.")
+}
