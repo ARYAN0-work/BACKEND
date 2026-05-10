0089b0191da3a7fd7533450dcd65a3da74398eda
Aryan Mishra <carrer1270@gmail.com>
2026-05-10 23:58:29 +0530

    adding notes related to mongo

---
+++ MONGO_DB/app.js
@@ -0,0 +1,11 @@
+/*
+nosqul , non relational DB
+store JSON
+easy to get started
+free cloud hosting - ATLAS
+
+*/
+
+// we made connection to our database by creating a array maniupulating it and sending the manipulated array so this time we are gonna connect to proper database 
+
+// here in traditional database where you have rows and columns here you can store everything as json only and it basically didnt care how data relate to each other intead of tables we have collection which represnt group of item and instead of rows we have docs which represnt single item and a doc is a set of key value pairs
